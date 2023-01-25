9305a8273cfada3daab301a113b677812ec4b763
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-01-24 19:01:52 -0800

    Updated

--- write_handlers.c
+++ write_handlers.c
@@ -19,8 +19,8 @@ int handle_write_char(char c, char buffer[],
 { /* char is stored at left and paddind at buffer's right */
 	int i = 0;
 	char padd = ' ';
-
-	UNUSED(preciion);
+	
+	UNUSED(precision);
 	UNUSED(size);
 
 	if (flags & F_ZERO)
@@ -29,7 +29,7 @@ int handle_write_char(char c, char buffer[],
 	buffer[i] = '\0';
 
 	if (width > 1)
-
+	{
 		buffer[BUFF_SIZE - 1] = '\0';
 		for (i = 0; i < width - 1; i++)
 			buffer[BUFF_SIZE - i - 2] = padd;
@@ -37,8 +37,8 @@ int handle_write_char(char c, char buffer[],
 			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
-		return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-g			write(1, &buffer[0], 1));
+		return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) + 
+				write(1, &buffer[0], 1));
 	}
 	return (write(1, &buffer[0], 1));
 }
@@ -167,7 +167,7 @@ int write_unsgnd(int is_negative, int ind,
 	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
 		return (0); /* printf(".0d", 0)  no char is printed */
 
-\	if (precision > 0 && precision < length)
+	if (precision > 0 && precision < length)
 		padd = ' ';
 
 	while (precision > length)
